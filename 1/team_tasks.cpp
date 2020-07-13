530ae391e09535015d78cb56515a1fd8f365ff10
Maksim Masniy <mmasniy@e1r3p8.unit.ua>
2020-07-13 15:28:32 +0300

    tuple, pair - DONE

--- 1/team_tasks.cpp
+++ 1/team_tasks.cpp
@@ -8,28 +8,20 @@
 using namespace std;
 
 // Перечислимый тип для статуса задачи
-enum class TaskStatus {
-    NEW,          // новая
-    IN_PROGRESS,  // в разработке
-    TESTING,      // на тестировании
-    DONE          // завершена
-};
+//enum class TaskStatus {
+//    NEW,          // новая
+//    IN_PROGRESS,  // в разработке
+//    TESTING,      // на тестировании
+//    DONE          // завершена
+//};
 
 // Объявляем тип-синоним для map<TaskStatus, int>,
 // позволяющего хранить количество задач каждого статуса
-using TasksInfo = map<TaskStatus, int>;
+//using TasksInfo = map<TaskStatus, int>;
 
 class TeamTasks {
     map<string, TasksInfo> team_tasks;
 
-    int GetAllTasks(TasksInfo& tasks_person) {
-        int count = 0;
-
-        for (int t = 0; t < 4; ++t) {
-            count += tasks_person[static_cast<TaskStatus>(t)];
-        }
-        return count;
-    }
 public:
     // Получить статистику по статусам задач конкретного разработчика
     const TasksInfo& GetPersonTasksInfo(const string& person) const {
@@ -45,49 +37,63 @@ public:
     // подробности см. ниже
     tuple<TasksInfo, TasksInfo> PerformPersonTasks(
             const string& person, int task_count) {
-        TasksInfo before_tasks = team_tasks.at(person);
-        TasksInfo after_tasks;
-        int all_task = GetAllTasks(before_tasks);
-        if (task_count > all_task) {
-            task_count = all_task;
+        TasksInfo before_tasks, after_tasks, buffer;
+
+        if (!team_tasks.count(person)) {
+            return tie(after_tasks, before_tasks);
         }
-        all_task = 0;
-//        for (auto& t : before_tasks) {
-//            if ((int) t.first == before_tasks[TaskStatus::NEW]) {
-//                (int) t.second--;
-//                all_task++;
-//            }
-//            else if (all_task == task_count) {
-//                break;
-//            }
-//        }
-        (void) task_count;
-        cout << "All_Task : " << all_task << endl;
-        return make_tuple(before_tasks, after_tasks);
+        buffer = team_tasks.at(person);
+//        cout << "TASK_COUNT: " << task_count;
+//        PrintMap(before_tasks, "before");
+//        PrintMap(after_tasks, "after");
+//        PrintMap(buffer, "buffer");
+//        cout << "---------------------------------" << endl;
+        for (const auto& [key, value] : team_tasks[person]) {
+            if (key != TaskStatus::DONE) {
+                for (int i = 0; i < value; ++i) {
+                    auto possition = static_cast<int>(key) + 1;
+                    if (task_count) {
+                        --buffer[key];
+                        if (buffer[key] == 0) {
+                            buffer.erase(key);
+                        }
+                        ++buffer[static_cast<TaskStatus>(possition)];
+                        ++after_tasks[static_cast<TaskStatus>(possition)];
+                        task_count--;
+                    } else {
+                        ++before_tasks[key];
+                    }
+                }
+            }
+        }
+        team_tasks.at(person) = buffer;
+//        PrintMap(before_tasks, "before");
+//        PrintMap(after_tasks, "after");
+//        PrintMap(buffer, "buffer");
+        return make_tuple(after_tasks, before_tasks);
     }
 };
 
 // Принимаем словарь по значению, чтобы иметь возможность
 // обращаться к отсутствующим ключам с помощью [] и получать 0,
 // не меняя при этом исходный словарь
-void PrintTasksInfo(TasksInfo tasks_info) {
-    cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
-         ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
-         ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
-         ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
-}
+//void PrintTasksInfo(TasksInfo tasks_info) {
+//    cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+//         ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+//         ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+//         ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+//}
 
-int main() {
-    TeamTasks tasks;
+//int main() {
+//    TeamTasks tasks;
 //    tasks.AddNewTask("Ilia");
-    for (int i = 0; i < 3; ++i) {
-        tasks.AddNewTask("Ivan");
-    }
-//      cout << "Ilia's tasks: ";
-//      PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-        cout << "Ivan's tasks: ";
-        PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
-//        tasks.PerformPersonTasks("Ivan", 2);
+//    for (int i = 0; i < 3; ++i) {
+//        tasks.AddNewTask("Ivan");
+//    }
+//    cout << "Ilia's tasks: ";
+//    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+//    cout << "Ivan's tasks: ";
+//    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
 //
 //    TasksInfo updated_tasks, untouched_tasks;
 //
@@ -104,9 +110,10 @@ int main() {
 //    PrintTasksInfo(updated_tasks);
 //    cout << "Untouched Ivan's tasks: ";
 //    PrintTasksInfo(untouched_tasks);
+//
+//    return 0;
+//}
 
-    return 0;
-}
 
 //
 // Created by mmasniy on 11.07.2020.

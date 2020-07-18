3aaa4fadbc7615d608ce7ecdac8a7ceedb3d86a9
mmasniy <masnijmaksim@gmail.com>
2020-07-18 08:17:51 +0300

    Dir 3

---
+++ 3/sum_reverse_sort.h
@@ -0,0 +1,18 @@
+//
+// Created by Maksim Masniy on 7/17/20.
+//
+
+#ifndef INC_1_SUM_REVERSE_SORT_H
+#define INC_1_SUM_REVERSE_SORT_H
+
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int Sum(int x, int y);
+string Reverse(string s);
+void Sort(vector<int>& nums);
+
+#endif //INC_1_SUM_REVERSE_SORT_H

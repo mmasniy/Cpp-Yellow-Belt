f2f89178ec80f26935df543da2dfd8d39226e529
mmasniy <masnijmaksim@gmail.com>
2020-07-18 08:09:42 +0300

    phone number - valid

--- 2/phone_number.cpp
+++ 2/phone_number.cpp
@@ -2,7 +2,32 @@
 #include <sstream>
 #include "phone_number.h"
 
+void GetNumber(stringstream& number, string& string_code, const char delim = '\n') {
+    getline(number, string_code, delim);
+    if (string_code.empty()) {
+        throw invalid_argument("Not found need delimeter");
+    }
+}
+
 PhoneNumber::PhoneNumber(const string &international_number) {
+    stringstream number;
+    number << international_number;
+    if (number) {
+        if (number.peek() != '+') {
+            throw invalid_argument("String does not start with +");
+        }
+        number.ignore(1);
+        GetNumber(number, country_code_, '-');
+    }
+    if (number) {
+        GetNumber(number, city_code_, '-');
+    }
+    if (number) {
+        GetNumber(number, local_number_);
+    }
+    else {
+        throw invalid_argument("String isn`t valid");
+    }
 
 }
 
@@ -22,4 +47,8 @@ string PhoneNumber::GetInternationalNumber()  const{
     return "+" + country_code_ + "-" + city_code_ + "-" + local_number_;
 }
 
+int main() {
+    PhoneNumber num ("+380-68-");
 
+    cout << num.GetInternationalNumber() << endl;
+}
